f795bfade4018915b49fa79d72f606b58238b0c2
talis <talissonv57@gmail.com>
2024-09-02 19:58:12 -0300

    aula6

---
+++ aula06.py
@@ -0,0 +1,8 @@
+# Definindo algumas variáveis
+idade = 25
+tem_carteira_motorista = True
+# Usando o operador lógico 'and' para verificar múltiplas
+if idade >= 18 and tem_carteira_motorista:
+    print("Você pode alugar um carro.")
+else:
+    print("Você não pode alugar um carro.")
