7e952b307980be97d3ba879ea3039fa8a85bda24
talis <talissonv57@gmail.com>
2024-11-04 19:32:53 -0300

    aula7....20

--- lista_Ex_prof/ex_11.py
+++ lista_Ex_prof/ex_11.py
@@ -1,16 +1,28 @@
-numeros = [6, 23, 35, 96, 14, 9, 10, 17, 28, 58, 18, 3, 68, 87, 29, 6]
-maior_numero = numeros[0]
-menor_numero = numeros[0]
-indice_maior = 0
-indice_menor = 0
+numeros = [12, 23, 45, 32, 5, 87, 41, 98, 56, 39, 23, 75, 79, 99, 103,
+           42, 36, 68, 19, 28, 6, 32, 45, 12, 9, 10, 17, 28, 53, 18, 7, 26]
 
-for i in range(1, len(numeros)):
-    if numeros[i] > maior_numero:
-        maior_numero = numeros[i]
-        indice_maior = i
-    if numeros[i] < menor_numero:
-        menor_numero = numeros[i]
-        indice_menor = i
+pares = []
+impares = []
+soma_total = 0
+qtd_pares = 0
+qtd_impares = 0
 
-print(f'Maior número: {maior_numero}, Índice: {indice_maior}')
-print(f'Menor número: {menor_numero}, Índice: {indice_menor}')
+for num in numeros:
+
+    soma_total += num
+
+    if num % 2 == 0:
+        pares.append(num)
+        qtd_pares += 1
+    else:
+        impares.append(num)
+        qtd_impares += 1
+
+media = soma_total / len(numeros)
+
+print("Números pares:", pares)
+print("Números ímpares:", impares)
+print("Quantidade de números pares:", qtd_pares)
+print("Quantidade de números ímpares:", qtd_impares)
+print("Soma de todos os números:", soma_total)
+print("Média de todos os números:", media)
--- lista_Ex_prof/ex_19.py
+++ lista_Ex_prof/ex_19.py
@@ -1,17 +1,13 @@
-def calcular_media_ponderada():
-    nota1 = float(input('Informe a primeira nota: '))
-    peso1 = float(input('Informe o peso da primeira nota: '))
+nota1 = float(input('Informe a primeira nota: '))
+peso1 = float(input('Informe o peso da primeira nota: '))
 
-    nota2 = float(input("Informe a segunda nota: "))
-    peso2 = float(input('Informe o peso da segunda nota: '))
+nota2 = float(input("Informe a segunda nota: "))
+peso2 = float(input('Informe o peso da segunda nota: '))
 
-    nota3 = float(input('Informe a terceira nota: '))
-    peso3 = float(input('Informe o peso da terceira nota: '))
+nota3 = float(input('Informe a terceira nota: '))
+peso3 = float(input('Informe o peso da terceira nota: '))
 
-    soma_pesos = peso1 + peso2 + peso3
-    media_ponderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / soma_pesos
+soma_pesos = peso1 + peso2 + peso3
+media_ponderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / soma_pesos
 
-    print(f'A média ponderada é: {media_ponderada:.2f}')
-
-
-calcular_media_ponderada()
+print(f'A média ponderada é: {media_ponderada:.2f}')
